7c5f2783d6c22fa71315caf42828e88ee9c06f34
Ramya Bhukya <Ramya Bhukya@LAPTOP-PG58VPJS>
2024-06-29 09:40:45 +0530

    product Pom class
    added new ele2

--- ComCastCRMGUIFramework/src/main/java/com/comcast/crm/objectrepositoryutility/Products.java
+++ ComCastCRMGUIFramework/src/main/java/com/comcast/crm/objectrepositoryutility/Products.java
@@ -9,4 +9,8 @@ public class Products {
 	@FindBy(xpath = "//input[@alt='Create Product...']")
 	private WebElement createProductImgBtn;
 
+	
+	@FindBy(name = "search")
+	private WebElement ele2;
+
 }
